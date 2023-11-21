584030e5e636de7052198cce4bcfd049bdb301a6
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2023-11-21 19:49:17 -0300

    added menu grid

--- composeApp/src/commonMain/kotlin/com/thondigital/nc/presentation/ui/components/Menu.kt
+++ composeApp/src/commonMain/kotlin/com/thondigital/nc/presentation/ui/components/Menu.kt
@@ -1,7 +1,9 @@
 package com.thondigital.nc.presentation.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -12,9 +14,13 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun Menu() {
     LazyVerticalGrid(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth().height(
+            400.dp
+        ),
         contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-        columns = GridCells.Adaptive(minSize = 100.dp)
+        columns = GridCells.Adaptive(minSize = 100.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalArrangement = Arrangement.Center
     ) {
 
         items((1..9).toList()) {
--- composeApp/src/commonMain/kotlin/com/thondigital/nc/presentation/ui/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/thondigital/nc/presentation/ui/home/HomeScreen.kt
@@ -1,6 +1,9 @@
 package com.thondigital.nc.presentation.ui.home
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
 import com.thondigital.nc.presentation.ui.components.Menu
 
@@ -9,6 +12,12 @@ object HomeScreen : Screen {
     @Composable
     override fun Content() {
 //        val screenModel = getScreenModel<HomeScreenModel>()
-        Menu()
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) { ->
+            item {
+                Menu()
+            }
+        }
     }
 }
