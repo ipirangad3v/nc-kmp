c049833617b77eac36aee143d1ae1b21d9188ac9
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2023-11-24 15:39:11 -0300

    added material 3

--- composeApp/src/commonMain/kotlin/com/thondigital/nc/presentation/ui/components/Loading.kt
+++ composeApp/src/commonMain/kotlin/com/thondigital/nc/presentation/ui/components/Loading.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
