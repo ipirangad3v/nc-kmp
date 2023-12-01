e92c7c077fbc1da5326697f9fca5187388c98941
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2023-12-01 11:34:47 -0300

    runned ktlint

--- ncApp/src/iosMain/kotlin/MainViewController.kt
+++ ncApp/src/iosMain/kotlin/MainViewController.kt
@@ -2,8 +2,8 @@ import androidx.compose.ui.window.ComposeUIViewController
 import platform.UIKit.UIScreen
 import platform.UIKit.UIUserInterfaceStyle
 
-@Suppress("unused")
 // ktlint-disable standard:function-naming
+@Suppress("unused")
 fun MainViewController() =
     ComposeUIViewController {
         val isDarkTheme =
