58880e794488c6f19b039d4a5e38cf5c9b4f7176
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2024-01-08 19:52:14 -0300

    updated kmule version

--- ncApp/src/commonMain/kotlin/com/thondigital/nc/network/connectivity/DefaultConnectivityChecker.kt
+++ ncApp/src/commonMain/kotlin/com/thondigital/nc/network/connectivity/DefaultConnectivityChecker.kt
@@ -1,10 +1,23 @@
 package com.thondigital.nc.network.connectivity
 
+import Kmule.networkStatus
 import com.thondigital.nc.data.connectivity.ConnectivityChecker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class DefaultConnectivityChecker : ConnectivityChecker {
-    override fun isNetworkAvailable(): Boolean {
-//        ConnectivityStatus()
-        return true
+    private var isNetworkAvailable = false
+    private val scope = CoroutineScope(Dispatchers.Main)
+
+    init {
+        scope.launch {
+            networkStatus.collect {
+                isNetworkAvailable = it
+            }
+        }
     }
+
+
+    override fun isNetworkAvailable(): Boolean = isNetworkAvailable
 }
